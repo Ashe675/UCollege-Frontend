dd5a1bab3f137ed64ee4d7293133e474e67e59ee
jorgeco98 <jorgecoello1998@hotmail.com.com>
2024-07-13 21:45:06 -0600

    cambio en carrusel

--- src/components/admission/ListFacultyLogo.tsx
+++ src/components/admission/ListFacultyLogo.tsx
@@ -1,20 +1,44 @@
 import { Carousel } from "flowbite-react";
 
 export function ListFacultyLogo() {
+  const slides = [
+    [
+      "/Admissions/Ingenieria-color.png",
+      "/Admissions/Ciencias-Medicas-color.png",
+      "/Admissions/Odontologia-color.png",
+      "/Admissions/Quimica-y-Farmacia-color.png"
+    ],
+    [
+      "/Admissions/Humanidades-y-Artes-color.png",
+      "/Admissions/Ciencias-Sociales-color.png",
+      "/Admissions/Ciencias-Espaciales-color.png",
+      "/Admissions/Ciencias-Juridicas-color.png"
+    ],
+    [
+      "/Admissions/Ciencias-Economicas-color.png",
+      "/Admissions/Ciencias-color.png"
+    ]
+  ];
+
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel slideInterval={6000}>
-        <img src="/Admissions/pumas.png" alt="Pumas" style={{width:"15%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Ingenieria-color.png" alt="Facultad" style={{width:"25%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Ciencias-Medicas-color.png" alt="Facultad" style={{width:"18%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Odontologia-color.png" alt="Facultad" style={{width:"25%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Quimica-y-Farmacia-color.png" alt="Facultad" style={{width:"25%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Humanidades-y-Artes-color.png" alt="Facultad" style={{width:"25%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Ciencias-Sociales-color.png" alt="Facultad" style={{width:"25%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Ciencias-Espaciales-color.png" alt="Facultad" style={{width:"25%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Ciencias-Juridicas-color.png" alt="Facultad" style={{width:"25%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Ciencias-Economicas-color.png" alt="Facultad" style={{width:"25%", marginTop:"0", alignItems:"center"}}/>
-        <img src="/Admissions/Ciencias-color.png" alt="Facultad" style={{width:"25%", marginTop:"0", alignItems:"center"}}/>
+      <Carousel slideInterval={2000}>
+        {slides.map((slide, index) => (
+          <div key={index} className="flex justify-center items-center space-x-4">
+            {slide.map((src, idx) => (
+              <img
+                key={idx}
+                src={src}
+                alt="Facultad"
+                style={{
+                  width: src.includes("Ciencias-Medicas-color") ? "15%" : "20%",
+                  marginTop: "0",
+                  alignItems: "center"
+                }}
+              />
+            ))}
+          </div>
+        ))}
       </Carousel>
     </div>
   );
