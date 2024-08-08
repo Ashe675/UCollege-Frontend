93f22ad5e3b04173a7b96d5241e029705a8d8f31
Jose Cerrato <josecerrato675@gmail.com>
2024-08-08 09:24:25 -0600

    vista estadistias a medio palo

--- src/views/private/department_head/StatsView.tsx
+++ src/views/private/department_head/StatsView.tsx
@@ -2,11 +2,14 @@ import { getClassesStats } from "@/api/department_head/DepartmentHeadApi";
 import Dashboard from "@/components/department_head/DashBoard";
 
 import SpinnerFull from "@/components/spinner/SpinnerFull";
+import { useUserStore } from "@/stores/userStore";
 import { useQuery } from "@tanstack/react-query";
 
 export default function StatsView() {
+  const user = useUserStore(state => state.user)
+
   const { data, isLoading } = useQuery({
-    queryKey: ["stats"],
+    queryKey: ["stats", user.id],
     queryFn: getClassesStats,
     retry: false,
   });
